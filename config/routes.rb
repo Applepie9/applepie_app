b98c9e976a7328a8118cfc644d78d386be454a57
Applepie9 <horel.dora@gmail.com>
2021-09-28 17:59:32 +0200

    Add user session buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # get 'home/index'
-  root 'home#index'
-  get  'home/about'
+  root   'home#index'
+  get    '/users/sign_up',   to: 'devise/registrations#new'
+  get    '/users/sign_in',   to: 'devise/sessions#new'
+  post   '/users/sign_in',   to: 'devise/sessions#create'
+  delete '/users/sign_out',  to: 'devise/sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
