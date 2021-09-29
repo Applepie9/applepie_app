5199d89ff29e4ff31bfbfe35ddf7834e00da84dc
Applepie9 <horel.dora@gmail.com>
2021-09-29 15:49:03 +0200

    Fix sign out bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
   # get 'home/index'
   root   'home#index'
-  get    '/users/sign_up',   to: 'devise/registrations#new'
-  get    '/users/sign_in',   to: 'devise/sessions#new'
-  post   '/users/sign_in',   to: 'devise/sessions#create'
-  delete '/users/sign_out',  to: 'devise/sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
