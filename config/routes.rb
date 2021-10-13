81b8a5fbfe16c6fb2cf8069a83558d9534028ffd
Applepie9 <horel.dora@gmail.com>
2021-10-13 19:23:53 +0200

    Add create, update, destroy recipes actions

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   get    'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :recipes, only: [:index, :show]
+  resources :recipes
 end
