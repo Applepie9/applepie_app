2123aad8b86ec86dcb1061ab7804a661abc672fc
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2021-12-19 07:31:41 +0100

    add registration controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,56 @@
+module Api
+  class UsersController < Api::ApplicationController
+    skip_before_action :doorkeeper_authorize!, only: %i[create]
+
+    def create
+      user = User.new(
+        email: user_params[:email], 
+        password: user_params[:password], 
+        password_confirmation: user_params[:password_confirmation]
+      )
+
+      client_app = Doorkeeper::Application.first
+
+      return render(json: { error: 'Invalid client ID'}, status: 403) if !client_app
+
+      if user.save
+        access_token = Doorkeeper::AccessToken.create(
+          resource_owner_id: user.id,
+          application_id: client_app.id,
+          refresh_token: generate_refresh_token,
+          expires_in: Doorkeeper.configuration.access_token_expires_in.to_i,
+          scopes: ''
+        )
+
+        render(json: {
+          user: {
+            id: user.id,
+            email: user.email,
+            access_token: access_token.token,
+            token_type: 'bearer',
+            expires_in: access_token.expires_in,
+            refresh_token: access_token.refresh_token,
+            created_at: access_token.created_at.to_time.to_i
+          }
+        })
+      else
+        render(json: { error: user.errors.full_messages }, status: 422)
+      end
+    end
+
+    private
+
+    def user_params
+      params.permit(:email, :password, :password_confirmation)
+    end
+
+    def generate_refresh_token
+      loop do
+        # generate a random token string and return it, 
+        # unless there is already another token with the same string
+        token = SecureRandom.hex(32)
+        break token unless Doorkeeper::AccessToken.exists?(refresh_token: token)
+      end
+    end 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
   # get 'home/index'
   root   'home#index'
   get    'home/about'
+  
+  namespace :api do
+    resources :users, only: %i[create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
