6719d966b92178f48578d6614f82ece1ec22d1f8
Applepie9 <horel.dora@gmail.com>
2021-12-28 21:31:37 +0100

    Add basic styling

--- client/src/components/ShowAll.js
+++ client/src/components/ShowAll.js
@@ -4,8 +4,11 @@ import { Card, CardGroup } from "react-bootstrap";
 
 export default function ShowAll() {
   return (
-    <div>
-      <Card className="bg-dark text-white">
+    <div style={{ margin: "0px 25px 50px 25px", padding: "10px" }}>
+      <Card
+        className="bg-dark text-white"
+        style={{ margin: "0px 0px 50px 0px", border: "15px", width: "500px" }}
+      >
         <Card.Img variant="top" src="holder.js/792px180" />
         <Card.Body>
           <Card.Text>
@@ -16,24 +19,24 @@ export default function ShowAll() {
       </Card>
 
       <div className="space-around">
-          <CardGroup className=" grid space-around card-list">
-            <CardComp
-              avatar={`https://www.nosalty.hu`}
-              recipename={"Applepie0"}
-            ></CardComp>
-            <CardComp
-              avatar={`https://www.streetkitchen.hu`}
-              recipename={"Applepie1"}
-            ></CardComp>
-            <CardComp
-              avatar={`https://www.streetkitchen.hu`}
-              recipename={"Applepie2"}
-            ></CardComp>
-            <CardComp
-              avatar={`https://www.streetkitchen.hu`}
-              recipename={"Applepie3"}
-            ></CardComp>
-          </CardGroup>
+        <CardGroup className=" grid space-around card-list">
+          <CardComp
+            avatar={`https://www.nosalty.hu`}
+            recipename={"Applepie0"}
+          ></CardComp>
+          <CardComp
+            avatar={`https://www.streetkitchen.hu`}
+            recipename={"Applepie1"}
+          ></CardComp>
+          <CardComp
+            avatar={`https://react-bootstrap.netlify.app/components/cards`}
+            recipename={"Applepie2"}
+          ></CardComp>
+          <CardComp
+            avatar={`https://universe.leagueoflegends.com/en_US/champions`}
+            recipename={"Applepie3"}
+          ></CardComp>
+        </CardGroup>
       </div>
     </div>
   );
--- client/src/components/Toolbar.js
+++ client/src/components/Toolbar.js
@@ -1,30 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { loggedIn } from "../utils/auth";
 import Logout from "./auth/Logout";
 import "../index.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
 
-// export default function Toolbar() {
-//   return (
-//     <nav >
-//       {loggedIn() ? (
-//         <Logout />
-//       ) : (
-//         <div>
-//           <Link to="login">Login</Link> <Link to="register">Register</Link>
-//         </div>
-//       )}
-//     </nav>
-//   );
-// }
-
 export default function Toolbar() {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" fixed="top" variant="dark" style={{padding: "10px", margin: "25px 25px 50px 25px"}}>
       <Container>
         <Navbar.Brand href="/">applepie-app</Navbar.Brand>
-        <Nav className="justify-content-end">
+        <Nav style={{float: "right"}}>
           <Nav.Item>
             {loggedIn() ? (
               <Logout />
